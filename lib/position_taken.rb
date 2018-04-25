da083635bbd394a50202292d621a7645e6be31ef
Pablo De La Cruz <pdelac@ramapo.edu>
2018-04-25 16:13:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,10 @@
 
 def position_taken? (board, index)
 
-  if board[index] != " " || board[index] != "" 
-     false
-   elsif board[index] == "X" || board[index] == "O"
+  if board[index] == "X" || board[index] == "O" 
      true
+   elsif board[index] != " " || board[index] != "" 
+     false
    else
      true
    end
